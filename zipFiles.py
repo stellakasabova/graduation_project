49eefc80d1c4c20786824cf13a61dfaf3f7cf641
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-01-17 01:36:18 +0200

    Changed variable names
    One of the variables was shadowing a built-in name

--- zipFiles.py
+++ zipFiles.py
@@ -4,9 +4,9 @@ from os.path import basename
 
 
 def zipFiles(path):
-    with ZipFile('test.zip', 'w') as zip:
+    with ZipFile('test.zip', 'w') as zip_object:
         for folder_name, sub_folders, files in os.walk(path):
             for filename in files:
                 file_path = os.path.join(folder_name, filename)
-                zip.write(file_path, basename(file_path))
-    zip.close()
+                zip_object.write(file_path, basename(file_path))
+    zip_object.close()
