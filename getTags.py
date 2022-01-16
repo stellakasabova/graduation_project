1450fa4fcf6d8d9fb19e240334ae351b098df7f1
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-01-17 01:38:03 +0200

    Updated algorithm
    The array that was being returned didn't contain the right values

--- getTags.py
+++ getTags.py
@@ -1,5 +1,10 @@
 import requests
 
+class Tag:
+    def __init__(self, name, confidence):
+        self.name = name
+        self.confidence = confidence
+
 def getTags(path):
     subscription_key_file = open("subscription_key.txt", "r")
     subscription_key = subscription_key_file.read()
@@ -13,4 +18,9 @@ def getTags(path):
 
     response_tags = requests.post(analyze_url, headers=headers, params=params_tags, data=data)
     tags = response_tags.json()
-    print("Tags: ", tags)
+    tag_arr = []
+    for i in range(0, len(tags["tags"])):
+        temp_tag = Tag(tags["tags"][i]["name"], tags["tags"][i]["confidence"])
+        tag_arr.append(temp_tag)
+
+    return tag_arr
