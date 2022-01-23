949bb3ffc2e7ff7a9cc396082692c6f280744893
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-01-23 23:54:51 +0200

    Shows tags of images
    When choosing a directory only the image files will be selected, the size limit for the Computer Vision API has been dealt with.

--- mainWIndow.py
+++ mainWIndow.py
@@ -3,12 +3,26 @@ import PIL
 from PIL import ImageTk, Image
 from tkinter import *
 from tkinter import filedialog
+from getTags import getTags
 
 root = Tk()
 root.title('Application')
 root.geometry("1500x1500")
 image_paths = []
 images = []
+tag_arr = []
+
+def getImageTags(path):
+    arr = getTags(path)
+    tags = []
+    for i in range(0, len(arr)):
+        empty_label = Label(root, text="")
+        tags.append(empty_label)
+
+    for j in range(0, len(tags)):
+        tags[j].configure(text=arr[j].name)
+
+    return tags
 
 def aa(path_param):
     global image_paths
@@ -16,18 +30,23 @@ def aa(path_param):
     global image_label
     global button_back
     global button_forward
+    global tag_arr
 
     extensions = ('.JPG', '.jpg', '.JPEG', '.jpeg', '.PNG', '.png')
-    for (root_, dirs, files) in os.walk(path_param):
-        for file_ in files:
-            if file_.endswith(extensions):
-                path = os.path.join(path_param, file_)
+    for file in os.listdir(path_param):
+        if file.endswith(extensions):
+            path = os.path.join(path_param, file)
+            if os.path.getsize(path) / (1024 * 1024) < 4:
                 image_paths.append(path)
 
+    tag_arr = getImageTags(image_paths[0])
+    for g in range(0, len(tag_arr)):
+        tag_arr[g].grid(row=g, column=0)
+
     for image in image_paths:
-        temp = PIL.Image.open(image)
-        temp = temp.resize((500, 500), PIL.Image.ANTIALIAS)
-        temp_resized = ImageTk.PhotoImage(temp)
+        temp_image = PIL.Image.open(image)
+        temp_image = temp_image.resize((500, 500), PIL.Image.ANTIALIAS)
+        temp_resized = ImageTk.PhotoImage(temp_image)
         images.append(temp_resized)
 
     image_label = Label(root, image=images[0])
@@ -42,9 +61,17 @@ def forward(image_number):
     global image_label
     global button_back
     global button_forward
+    global tag_arr
+
+    for h in range(0, len(tag_arr)):
+        tag_arr[h].grid_forget()
 
     image_label.grid_forget()
     image_label = Label(image=images[image_number - 1])
+
+    tag_arr = getImageTags(image_paths[image_number - 1])
+    for k in range(0, len(tag_arr)):
+        tag_arr[k].grid(row=k, column=0)
 
     button_back = Button(root, text="<<", command=lambda: back(image_number-1))
     button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
@@ -60,9 +87,17 @@ def back(image_number):
     global image_label
     global button_back
     global button_forward
+    global tag_arr
+
+    for u in range(0, len(tag_arr)):
+        tag_arr[u].grid_forget()
 
     image_label.grid_forget()
     image_label = Label(image=images[image_number - 1])
+
+    tag_arr = getImageTags(image_paths[image_number - 1])
+    for e in range(0, len(tag_arr)):
+        tag_arr[e].grid(row=e, column=0)
 
     button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
     button_forward = Button(root, text=">>", command=lambda: forward(image_number + 1))
