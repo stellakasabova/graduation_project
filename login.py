3c7986994bddb4b78b869386501c1cb018c9fa59
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-02-17 10:37:40 +0200

    Implemented adding of tags
    It's still in an initial stage, only sends filler content.

--- login.py
+++ login.py
@@ -1,39 +1,47 @@
 from tkinter import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-root = Tk()
-root.geometry("100x100")
-
-def logIn(input1, input2):
-    username = input1.get()
-    password = input2.get()
-
+def logIn(username_input, password_input, paths):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://pixabay.com/accounts/login/')
 
-    element1 = driver.find_element(by=By.NAME, value='username')
+    element1 = driver.find_element(By.NAME, 'username')
     element1.clear()
-    element1.send_keys(username)
+    element1.send_keys(username_input.get())
 
-    element2 = driver.find_element(by=By.NAME, value='password')
+    element2 = driver.find_element(By.NAME, 'password')
     element2.clear()
-    element2.send_keys(password)
+    element2.send_keys(password_input.get())
 
-    driver.find_element(by=By.NAME, value='login').click()
+    driver.find_element(By.NAME, 'login').click()
     driver.get('https://pixabay.com/accounts/media/')
 
     driver.get('https://pixabay.com/accounts/media/upload/')
-    driver.find_element(by=By.CLASS_NAME, value='dialog-container').find_element(by=By.NAME, value='ownership').click()
-    driver.find_element(by=By.CLASS_NAME, value='dialog-container').find_element(by=By.NAME, value='license').click()
-    driver.find_element(by=By.CLASS_NAME, value='dialog-container').find_element(by=By.CLASS_NAME, value='primary-button').click()
+    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.NAME, 'ownership').click()
+    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.NAME, 'license').click()
+    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.CLASS_NAME, 'primary-button').click()
 
-    files = driver.find_element(by=By.CLASS_NAME, value='dz-hidden-input')
-    files.send_keys('D:/photos/Upload/IMG_0386.JPG\nD:/photos/Upload/IMG_0389.png')
-    files.submit()
+    big_string = paths[0] + '\n' + paths[1] + '\n' + paths[2]
+    files = driver.find_element(By.CLASS_NAME, 'dz-hidden-input')
+    files.send_keys(big_string)
 
-def upload():
+    time.sleep(30)
+
+    tags = driver.find_elements(By.CLASS_NAME, 'tagify__input')
+    print("found")
+    for element in tags:
+        ActionChains(driver).move_to_element(element)
+        element.clear()
+        element.send_keys("smth")
+        print("in loop")
+
+    time.sleep(40)
+
+def upload(paths):
     upload_window = Toplevel(height=300, width=300)
     username_label = Label(upload_window, text="Username:")
     username_label.pack()
@@ -45,9 +53,6 @@ def upload():
     password_input = Entry(upload_window, show="*")
     password_input.pack()
 
-    upload_func_button = Button(upload_window, text="Upload", command=lambda: logIn(username_input, password_input))
+    upload_func_button = Button(upload_window, text="Upload", command=lambda: logIn(username_input, password_input, paths))
     upload_func_button.pack()
 
-bttn = Button(root, text="upload", command=upload).grid(row=0, column=0)
-
-root.mainloop()
