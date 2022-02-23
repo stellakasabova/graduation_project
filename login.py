f8bd96bfd2f2ea70da89968962520207939a5e37
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-02-23 23:06:03 +0200

    Replaced Selenium upload with FTP
    After problems arose with Selenium I decided to switch to FTP uploading(on a personal server for now for testing).

--- login.py
+++ login.py
@@ -1,47 +1,23 @@
 from tkinter import *
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+import ftplib
+import os
 
-def logIn(username_input, password_input, paths):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get('https://pixabay.com/accounts/login/')
+def logIn(username_input, password_input, path):
+    address = 'A32900B79A49CDDDE121D03A004BB565B.asuscomm.com'
+    session = ftplib.FTP(address)
+    session.login(username_input.get(), password_input.get())
+    session.cwd('LINKSYS/stella')
 
-    element1 = driver.find_element(By.NAME, 'username')
-    element1.clear()
-    element1.send_keys(username_input.get())
+    extensions = ('.JPG', '.jpg', '.JPEG', '.jpeg', '.PNG', '.png')
+    for file in os.listdir(path):
+        if file.endswith(extensions):
+            img_path = os.path.join(path, file)
+            session.storbinary('STOR ' + file, open(img_path, 'rb'))
 
-    element2 = driver.find_element(By.NAME, 'password')
-    element2.clear()
-    element2.send_keys(password_input.get())
+    session.quit()
+    print("success")
 
-    driver.find_element(By.NAME, 'login').click()
-    driver.get('https://pixabay.com/accounts/media/')
-
-    driver.get('https://pixabay.com/accounts/media/upload/')
-    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.NAME, 'ownership').click()
-    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.NAME, 'license').click()
-    driver.find_element(By.CLASS_NAME, 'dialog-container').find_element(By.CLASS_NAME, 'primary-button').click()
-
-    big_string = paths[0] + '\n' + paths[1] + '\n' + paths[2]
-    files = driver.find_element(By.CLASS_NAME, 'dz-hidden-input')
-    files.send_keys(big_string)
-
-    time.sleep(30)
-
-    tags = driver.find_elements(By.CLASS_NAME, 'tagify__input')
-    print("found")
-    for element in tags:
-        ActionChains(driver).move_to_element(element)
-        element.clear()
-        element.send_keys("smth")
-        print("in loop")
-
-    time.sleep(40)
-
-def upload(paths):
+def uploadTopLevel(paths):
     upload_window = Toplevel(height=300, width=300)
     username_label = Label(upload_window, text="Username:")
     username_label.pack()
