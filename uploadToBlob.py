949e3e6d5b2f393ae7ad8b76e5e786643bba17ad
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-03-06 23:21:09 +0200

    Updated archiving
    Added the option to choose a name for the .zip file.

--- uploadToBlob.py
+++ uploadToBlob.py
@@ -1,19 +1,19 @@
 import logging
+import os
 
 from azure.core.exceptions import HttpResponseError
 
 from zipFiles import zipFiles
 from azure.storage.blob import BlobServiceClient
-import os
 from tkinter import messagebox
 
-def upload_blob(path):
+def upload_blob(path, zip_name):
     connection = open("connection_string.txt", "r")
     connection_string = connection.read()
     blob_container = "blob"
 
-    zipFiles(path)
-    file_name = 'archive.zip'
+    zipFiles(path, zip_name)
+    file_name = zip_name+'.zip'
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container=blob_container, blob=file_name)
 
@@ -22,7 +22,7 @@ def upload_blob(path):
 
     logger.info("Attempting archiving")
     try:
-        with open(os.path.join(path, 'archive.zip'), "rb") as data:
+        with open(os.path.join(path, file_name), "rb") as data:
             blob_client.upload_blob(data, overwrite=True)
     except HttpResponseError:
         logger.warning("Archiving failed")
--- zipFiles.py
+++ zipFiles.py
@@ -1,11 +1,11 @@
-from zipfile import ZipFile
 import os
 from os.path import basename
+from zipfile import ZipFile
 
-def zipFiles(path):
+def zipFiles(path, name):
     extensions = ('.JPG', '.jpg', '.JPEG', '.jpeg', '.PNG', '.png')
 
-    with ZipFile(path+'/archive.zip', 'w') as zip_object:
+    with ZipFile(path+'/'+name+'.zip', 'w') as zip_object:
         for file in os.listdir(path):
             if file.endswith(extensions):
                 file_path = os.path.join(path, file)
