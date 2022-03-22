eb3338a8702a27db539867806d0745f15aa779cd
Stella Kasabova <36966722+stellakasabova@users.noreply.github.com>
2022-03-22 22:33:24 +0200

    Added archive fetching

--- blobFunctions.py
+++ blobFunctions.py
@@ -22,17 +22,17 @@ def uploadBlob(path, zip_name):
 
     encryptKeys()
 
-    blob_container = "blob"
-
     zipFiles(path, zip_name)
     file_name = zip_name+'.zip'
+
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container=blob_container, blob=file_name)
+    blob_client = blob_service_client.get_blob_client(container="store", blob=file_name)
 
     logging.basicConfig(level=logging.DEBUG, filename='logs.log', format='%(asctime)s %(levelname)s:%(message)s')
     logger = logging.getLogger(__name__)
 
     logger.info("Attempting archiving")
+
     try:
         with open(os.path.join(path, file_name), "rb") as data:
             blob_client.upload_blob(data, overwrite=True)
@@ -41,5 +41,37 @@ def uploadBlob(path, zip_name):
     finally:
         logger.debug("Archiving done")
 
+    os.remove(path + "/" + file_name)
     messagebox.showinfo("Done", "Archiving done!")
 
+def downloadBlob(path, zip_name):
+    decrypted_data = str(decryptKeys())
+    decrypted_data.replace("'", '"')
+
+    result = json.loads(decrypted_data)
+    connection_string = result["connection"]
+
+    with open('sas_keys.json', 'w') as file:
+        file.write(decrypted_data)
+        file.close()
+
+    encryptKeys()
+
+    blob_name = zip_name+'.zip'
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container = blob_service_client.get_container_client("store")
+
+    logging.basicConfig(level=logging.DEBUG, filename='logs.log', format='%(asctime)s %(levelname)s:%(message)s')
+    logger = logging.getLogger(__name__)
+
+    logger.info("Attempting download")
+
+    try:
+        with open(os.path.join(path, blob_name), "wb") as file:
+            file.write(container.get_blob_client(blob_name).download_blob().readall())
+    except HttpResponseError:
+        logger.warning("Download failed")
+    finally:
+        logger.debug("Download done")
+
+    messagebox.showinfo("Done", "Download done!")
